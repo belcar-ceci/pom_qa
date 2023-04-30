7ecb88655dea6df90dad5b39b2fe5352c5c4e429
Cecilia Carbajal <cecilia.carbajal@sportium.com>
2023-05-01 00:20:17 +0200

    add second command chrome browser

--- src/test/java/com/hiberus/university/cecilia/maven/first/CommandsBrowser.java
+++ src/test/java/com/hiberus/university/cecilia/maven/first/CommandsBrowser.java
@@ -2,23 +2,27 @@ package com.hiberus.university.cecilia.maven.first;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class CommandsBrowser {
 
-    public static void main(String[] args) throws InterruptedException{
+    public static void main(String[] args) {
 
         WebDriver driver;
-        WebDriverManager.firefoxdriver().setup();
-        FirefoxOptions firefoxOptions = new FirefoxOptions();
-        driver = new FirefoxDriver(firefoxOptions);
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
 
-        driver.get("https://www.google.com");
+        driver.get("https://www.hiberus.com/");
+
+        String title = driver.getTitle();
+        System.out.println(title);
 
         //Thread.sleep(5000);
-        driver.quit();
+        //driver.quit();
 
     }
 
