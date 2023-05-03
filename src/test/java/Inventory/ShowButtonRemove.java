26154f059dd5b9ae7586d763736104ae928cd55b
Cecilia Carbajal <cecilia.carbajal@sportium.com>
2023-05-04 01:00:59 +0200

    adding class button remove for test

---
+++ src/test/java/Inventory/ShowButtonRemove.java
@@ -0,0 +1,14 @@
+package Inventory;
+
+public class ShowButtonRemove {
+    public static void main(String[] args) {
+        /*
+        1. Ir a la página https://www.saucedemo.com
+        2. Escribir el username standard_user
+        3. Escribir el password secret_sauce
+        4. Pulsar en el botón del Login.
+        5. Agregar al carrito el producto Sauce Labs Onesie
+        //Xpath //button[@id='add-to-cart-sauce-labs-onesie']
+        6. Validamos que, al agregar el producto, se visualiza el botón REMOV*/
+    }
+}
