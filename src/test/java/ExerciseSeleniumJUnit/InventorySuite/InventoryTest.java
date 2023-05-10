02745e13017740bf77fffd5029081c6a03fe392c
Cecilia Carbajal <cecilia.carbajal@sportium.com>
2023-05-11 01:34:49 +0200

    adding test remove product inventory

--- src/test/java/ExerciseSeleniumJUnit/InventorySuite/InventoryTest.java
+++ src/test/java/ExerciseSeleniumJUnit/InventorySuite/InventoryTest.java
@@ -97,6 +97,14 @@ public class InventoryTest {
 
     @Test
     public void AddProductInCart(){
+        /*
+        * 1. Ir a la página https://www.saucedemo.com
+        2. Escribir el username standard_user
+        3. Escribir el password secret_sauce
+        4. Pulsar en el botón del Login.
+        5. Agregar al carrito el producto Sauce Labs Bolt T-Shirt
+        6. Validar que, en el icono del carrito, se ha agregado 1 producto
+        * */
 
         //Step 2
         driver.findElement(By.xpath("//input[@data-test='username']")).sendKeys(username);
@@ -122,10 +130,46 @@ public class InventoryTest {
 
     }
 
+    @Test
+    public void RemoveProductCartInventory(){
+
+        //Step 2
+        driver.findElement(By.xpath("//input[@data-test='username']")).sendKeys(username);
+
+        //Step 3
+        driver.findElement(By.xpath("//input[@data-test='password']")).sendKeys(password);
+
+        //Step 4
+        driver.findElement(By.xpath("//input[@data-test='login-button']")).click();
+
+        //Step 5
+        WebElement addToCartButton = driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-bolt-t-shirt']"));
+        addToCartButton.click();
+
+        //Step 6
+        WebElement showRemoveButton = driver.findElement(By.xpath("//button[@id='remove-sauce-labs-bolt-t-shirt']"));
+        showRemoveButton.click();
+
+        //Step 7
+        try {
+            WebElement cartIcon = driver.findElement(By.xpath("//a[@class='shopping_cart_link']"));
+            String cartItemCount = cartIcon.getText();
+            if (cartItemCount.isEmpty()) {
+                System.out.println("The product has been removed from the cart.");
+            } else {
+                System.out.println("Error: The product has not been removed from the cart.");
+            }
+        } catch (Exception e) {
+            System.out.println("The product is not removed from the cart, there is an exception: " + e.getMessage());
+
+        }
+
+    }
+
     @After
     public void TearDown(){
         //step closure
-        driver.quit();
+        //driver.quit();
 
     }
 
